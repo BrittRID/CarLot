7509ff53e083d4f254c338a052778db87b74d963
Brittany Ridley <britt.ridley@outlook.com>
2022-03-23 21:36:52 -0500

    car

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+
+        public Car()
+        {
+            CarLot.numberOfCars++;
+            //Classname.StaticMember
+        }
+
+
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDriveable = false)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDriveable;
+        }
+
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine(EngineNoise);
+        }
+
+        public void MakeHonkNoise()
+        {
+           Console.WriteLine(HonkNoise);
+        }
+
+      
+    }
+}
---
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace CarLotSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO
+
+            //Create a seperate class file called Car
+            //Car shall have the following properties: Year, Make, Model, EngineNoise,  , IsDriveable
+            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+            //The methods should take one string parameter: the respective noise property
+
+
+            //Now that the Car class is created we can instanciate 3 new cars
+            //Set the properties for each of the cars
+            //Call each of the methods for each car
+
+            //*************BONUS*************//
+
+            // Set the properties utilizing the 3 different ways we learned about, one way for each car
+
+            CarLot lot = new CarLot();
+            
+
+            Car VW = new Car();
+            lot.CarList.Add(VW);
+            VW.Year = 2019;
+            VW.Make = "JETTA";
+            VW.Model = "JETTA LE";
+            VW.EngineNoise = "Vroom";
+            VW.HonkNoise = "Bleep Bleep";
+            VW.IsDriveable = true;
+
+          //  VW.MakeEngineNoise(VW.EngineNoise);
+           // VW.MakeHonkNoise(VW.HonkNoise);
+
+            //object initializer syntax
+            Car celica = new Car() { Year = 2019, Make = "Toyota", Model = "Celica", EngineNoise = "Nothing", HonkNoise = "WoW" };
+            lot.CarList.Add(celica);
+
+            celica.MakeEngineNoise();
+            celica.MakeHonkNoise();
+
+            //Constructor initializer
+            Car someOtherCar = new Car(3000, "Mars Inc.", "Alpha ++", "Space Noises", " Does a space ship sink");
+            lot.CarList.Add(someOtherCar);
+
+
+            someOtherCar.MakeEngineNoise();
+            someOtherCar.MakeHonkNoise();
+
+            Console.WriteLine("--------------");
+
+            //*************BONUS X 2*************//
+
+
+            foreach(var car in lot.CarList)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Year: {car.Year} Make: {car.Make} and Model: {car.Model}");
+            }
+
+            //Create a CarLot class
+            //It should have at least one property: a List of cars
+            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
+            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+           
+
+            Console.WriteLine($"Number of car created: {CarLot.numberOfCars}");
+
+            foreach(var car in lot.Cars)
+            {
+                Console.WriteLine($"Year: {car.Year} Make: {car.Make} Model: {car.Model} ");
+            }
+
+
+
+
+
+
+        }
+    }
+}
